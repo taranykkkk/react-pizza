07745631575c258a4a45bc503a0549d556af3937
Evgeniy <nikolajmihlkov43@gmail.com>
2024-03-28 18:08:35 +0200

    fixed render image cartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -44,7 +44,11 @@ const CartItemBlock: React.FC<CartItemProps> = ({
   return (
     <div className="cart__item">
       <div className="cart__item-img">
-        <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+        <img
+          className="pizza-block__image"
+          src={require(`../../assets/pizzas/${imageUrl}`)}
+          alt="Pizza"
+        />
       </div>
       <div className="cart__item-info">
         <h3>{title}</h3>
